71d27b24d4fd3e8b5136c79a24f0c4826ed69ad0
Eliot Jones <elioty@hotmail.co.uk>
2015-10-16 22:14:06 +0100

    add appveyor files remove nuget exe

---
+++ tools/ps/run-fixie-tests.ps1
@@ -0,0 +1,27 @@
+﻿$PackagesFolder = $env:APPVEYOR_BUILD_FOLDER + "\src\packages"
+
+$FixieFolder = Get-ChildItem $PackagesFolder -recurse | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "Fixie"}
+
+$LibFolder = $FixieFolder.FullName + "\lib"
+
+$SubFoldersOfLib = Get-ChildItem $LibFolder -Directory
+
+if($SubFoldersOfLib.Length -eq 0)
+{
+    echo "No folders in the fixie lib folder"
+    exit
+}
+
+$NetFolder = $SubFoldersOfLib[0].FullName
+
+$ExePath = $NetFolder + "\Fixie.Console.exe"
+
+$Command = $ExePath + " " + $env:APPVEYOR_BUILD_FOLDER + "\src\PostHaste.Tests\bin\Release\PostHaste.Tests.dll  --NUnitXml " + $env:APPVEYOR_BUILD_FOLDER + "\TestResult.xml" 
+
+echo "Running tests"
+
+iex $Command
+
+# upload results to AppVeyor
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ($env:APPVEYOR_BUILD_FOLDER + "\TestResult.xml"))
